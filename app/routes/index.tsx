f06f33dfe9e3963cc15f852fefbef415cce6ac30
Sarah Serussi <sarah.serussi@gmail.com>
2023-02-28 11:49:50 +0100

    adding some color to landing page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,27 +1,42 @@
 import { Link } from "@remix-run/react";
 
-import { useOptionalUser } from "~/utils";
-
 export default function Index() {
-  const user = useOptionalUser();
   return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      <div className="relative sm:pb-16 sm:pt-8">
-        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
-            <div className="absolute inset-0">
-              <div className="absolute inset-0 bg-[color:rgba(254,204,27,0.5)] mix-blend-multiply" />
-            </div>
-            <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-32">
-              <div className="mx-auto mt-16 max-w-7xl text-center">
-                <Link to="/posts" className="text-xl text-blue-600 underline">
-                  Blog Posts
-                </Link>
-              </div>
-            </div>
+    <main className="grid min-h-screen grid-rows-3 items-center bg-gradient-to-br from-blue via-green to-lime text-center">
+      <section className="header">
+        <h1 className="font-serif text-7xl">Læringshub</h1>
+      </section>
+      <section className="grid grid-cols-1 lg:grid-cols-4">
+        <div className="m-8 rounded-2xl bg-white p-8 shadow-xl">
+          <div className="m-16 text-center">
+            <Link to="/posts" className="text-blue-600 text-xl underline">
+              Bloggposter
+            </Link>
           </div>
         </div>
-      </div>
+        <div className="m-8 rounded-2xl bg-white p-8 shadow-xl">
+          <div className="m-16 text-center">
+            <Link to="/posts" className="text-blue-600 text-xl underline">
+              Kurs
+            </Link>
+          </div>
+        </div>
+        <div className="m-8 rounded-2xl bg-white p-8 shadow-xl">
+          <div className="m-16 text-center">
+            <Link to="/posts" className="text-blue-600 text-xl underline">
+              Foredrag
+            </Link>
+          </div>
+        </div>
+        <div className="m-8 rounded-2xl bg-white p-8 shadow-xl">
+          <div className="m-16 text-center">
+            <Link to="/posts" className="text-blue-600 text-xl underline">
+              Videoer
+            </Link>
+          </div>
+        </div>
+      </section>
+      <section className="footer">Footer</section>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {},
+    colors: {
+      black: "#000",
+      blue: "#1fb6ff",
+      purple: "#7e5bef",
+      pink: "rgba(244,2,144,1)",
+      orange: "rgba(254,109,1,1)",
+      green: "#00bf72",
+      lime: "#a8eb12",
+      yellow: "#ffc82c",
+      gray: "#8492a6",
+      lightgray: "#eee",
+      white: "#fafaf6",
+    },
+    extend: {
+      gridTemplateRows: {
+        // Complex site-specific row configuration
+        3: "200px minmax(900px, 1fr) 100px",
+      },
+    },
+    fontFamily: {
+      sans: ['"Open Sans"', "system-ui"],
+      serif: ['"Playfair_Display"', "ui-serif"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+      display: ["Oswald"],
+      body: ['"Open Sans"'],
+    },
   },
   plugins: [],
 };
