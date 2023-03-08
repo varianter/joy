9c86a0e25e7303272f98b065143b10c22aec3212
mamarku <mm@variant.no>
2023-03-08 09:00:07 +0100

    m

--- app/services/auth.server.ts
+++ app/services/auth.server.ts
@@ -1,17 +1,22 @@
 import { MicrosoftProfile, MicrosoftStrategy } from "remix-auth-microsoft";
 import { Authenticator } from "remix-auth";
 import { sessionStorage } from "~/services/session.server";
+import invariant from "tiny-invariant";
 
 export interface User {
   profile: MicrosoftProfile;
 }
 
+invariant(process.env.MICROSOFT_CLIENT_ID, "MICROSOFT_CLIENT_ID must be set");
+invariant(process.env.MICROSOFT_CLIENT_SECRET, "MICROSOFT_CLIENT_SECRET must be set");
+invariant(process.env.MICROSOFT_TENANT_ID, "MICROSOFT_TENANT_ID must be set");
+
 export let authenticator = new Authenticator<User>(sessionStorage);
 
 let microsoftStrategy = new MicrosoftStrategy(
   {
-    clientId: process.env.MICROSOFT_CLIENT_ID ?? "",
-    clientSecret: process.env.MICROSOFT_CLIENT_SECRET ?? "",
+    clientId: process.env.MICROSOFT_CLIENT_ID,
+    clientSecret: process.env.MICROSOFT_CLIENT_SECRET,
     redirectUri: "/auth/microsoft/callback",
     tenantId: process.env.MICROSOFT_TENANT_ID,
     scope: "openid profile",
