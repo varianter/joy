5b86775a4801a62094d9dc38de06cf17cc039bde
mamarku <mm@variant.no>
2023-03-13 19:11:16 +0100

    endre padding

--- app/components/buttons/PrimaryButton.tsx
+++ app/components/buttons/PrimaryButton.tsx
@@ -6,7 +6,7 @@ const PrimaryButton = (props: PrimaryButtonProps) => {
   const { text } = props;
 
   return (
-    <button className="group relative inline-flex items-center justify-start overflow-hidden rounded-3xl bg-variant-beige px-9 py-3 font-medium text-variant-blue transition-all  hover:bg-variant-secondary-beige">
+    <button className="group relative inline-flex hidden items-center justify-start overflow-hidden rounded-3xl bg-variant-beige px-9 py-3 text-xs font-medium text-variant-blue transition-all hover:bg-variant-secondary-beige  md:block">
       {text}
     </button>
   );
