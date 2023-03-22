9325f64d7c3107eb919371472bd41acaa214aa06
Linnealyngaas <Linnea.lyngaas@gmail.com>
2023-03-22 12:56:13 +0100

    Endret knapp

--- app/components/card/CourseCard.tsx
+++ app/components/card/CourseCard.tsx
@@ -15,31 +15,34 @@ const CourseCard = (props: CourseCardProps) => {
 
   return (
     <Card>
-      <div className="flex flex-col y-2">
-        <div className="flex justify-between">
-          <div className="inline-flex items-center"><img
-            alt="Kurs icon"
-            className="h-[1rem] pr-1.5"
-            src={"/assets/icons/course.svg"}
-          />
-            <p>Kurs</p>
+      <div className="flex flex-col y-2 justify-between h-full">
+        <div className="inline-flex flex-col">
+          <div className="flex justify-between">
+            <div className="inline-flex items-center"><img
+              alt="Kurs icon"
+              className="h-[1rem] pr-1.5"
+              src={"/assets/icons/course.svg"}
+            />
+              <p>Kurs</p>
+            </div>
+
+
+            <p className="text-variant-black">
+              {new Date(createdAt).toLocaleDateString("nb")}
+            </p>
           </div>
 
+          <img
+            alt={altImageText ?? "Figur av læreglede"}
+            className="h-[15rem] w-full py-3"
+            src={image ?? "/assets/default-article-image.svg"}
+          />
 
-          <p className="text-variant-black">
-            {new Date(createdAt).toLocaleDateString("nb")}
-          </p>
+          <h1 className="mt-2 text-left text-base">{title}</h1>
+          <p className="text-left pb-2">{description}</p>
         </div>
 
-        <img
-          alt={altImageText ?? "Figur av læreglede"}
-          className="h-[15rem] w-full py-3"
-          src={image ?? "/assets/default-article-image.svg"}
-        />
-
-        <h1 className="mt-2 text-left text-base">{title}</h1>
-        <p className="text-left pb-2">{description}</p>
-        <div className="inline-flex items-end justify-between leading-none">
+        <div className="inline-flex items-end justify-end">
           <a href={linkToCourse} target="_blank" rel="noreferrer">
             <AnimatedButton text="Ta kurset" />
 
