b9c14530df4d3ca4b73d11da5a1beb0c418da22c
Sarah Serussi <sarah.serussi@gmail.com>
2023-03-27 12:21:18 +0200

    extract layout to index page

--- app/components/card/NavigationCard.tsx
+++ app/components/card/NavigationCard.tsx
@@ -6,7 +6,7 @@ interface NavigationCardProps {
 const NavigationCard = (props: NavigationCardProps) => {
   const { header, icon } = props;
   return (
-    <article className="min-w-[15rem] overflow-hidden rounded-xl bg-variant-blue-2 p-4 text-white hover:bg-variant-blue">
+    <article className="min-w-[10rem] overflow-hidden rounded-xl bg-variant-blue-2 p-4 text-white hover:bg-variant-blue">
       <h1 className="ml-4 text-center text-lg">{header}</h1>
       <div className="flex justify-center">{icon}</div>
     </article>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,33 +1,100 @@
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import ArticlePreview from "~/components/ArticlePreview";
 import TagButton from "~/components/buttons/TagButton";
 import Card from "~/components/card/Card";
+import NavigationCard from "~/components/card/NavigationCard";
 import { getCategories } from "~/models/category.server";
-import { getNumNewestContent } from "~/models/content.server";
+import { getNumBlogposts, getNumNewestContent, getNumVideos } from "~/models/content.server";
 
 const numberOfNewContent = 2;
 
 export const loader = async () => {
-  const [newestContent, categories] = await Promise.all([
+  const [newestContent, categories, numVideos, numBlogposts] = await Promise.all([
     getNumNewestContent(numberOfNewContent),
     getCategories(),
+    getNumVideos(),
+    getNumBlogposts()
   ]);
   return json({
     newestContent,
     categories,
+    numVideos,
+    numBlogposts
   });
 };
 
 export default function Index() {
-  const { newestContent, categories } = useLoaderData<typeof loader>();
+  const { newestContent, categories, numVideos, numBlogposts } = useLoaderData<typeof loader>();
 
   return (
+      <div className="mt-24 md:mx-[5rem] md:mt-32 lg:mx-[15rem] xl:mx-[35rem]">
+        <section className="text-left">
+          <p className="text-2xl text-white">En variant av en</p>
+          <h2 className="text-5xl text-white">Læringshub</h2>
+          <p className="mt-4 text-white">
+            Vi i Variant lager og holder en del kurs og foredrag i flere sammenhenger. Vi har noen bloggposter her, noen YouTube-videoer der, og noen foredrag en annen plass. Dette har vi samlet i denne læringshuben.
+          </p>
+        </section>
+
+        <section className="my-8 grid grid-cols-2 gap-5 md:grid-cols-4">
+          <Link to="blogposts">
+            <NavigationCard
+              header={`Bloggposter (${numBlogposts})`}
+              icon={
+                <img
+                alt={"Figur av bloggpost"}
+                className="h-[3rem]"
+                src={"/assets/icons/blogpost.svg"}
+                />
+              }
+              />
+          </Link>
+
+          <Link to="lecture">
+            <NavigationCard
+              header="Foredrag (0)"
+              icon={
+                <img
+                alt={"Figur av foredrag"}
+                className="h-[3rem]"
+                src={"/assets/icons/lecture.svg"}
+                />
+              }
+              />
+          </Link>
+
+          <Link to="course">
+            <NavigationCard
+              header="Kurs (0)"
+              icon={
+                <img
+                  alt={"Figur av kurs"}
+                  className="h-[3rem]"
+                  src={"/assets/icons/course.svg"}
+                  />
+                }
+                />
+          </Link>
+
+          <Link to="videos">
+            <NavigationCard
+              header={`Videoer (${numVideos})`}
+              icon={
+                <img
+                alt={"Figur av video"}
+                className="h-[3rem]"
+                src={"/assets/icons/video.svg"}
+                />
+              }
+              />
+          </Link>
+        </section>
     <section>
-      <h1 className="mb-8 text-left text-4xl text-white md:text-5xl">
+      <h2 className="mb-8 text-left text-4xl text-white md:text-5xl">
         Bli inspirert 🤩
-      </h1>
-      <div className="">
+      </h2>
+      <div>
         {newestContent.map((content, index) => {
           return (
             <div key={content.id} className="my-5">
@@ -43,13 +110,13 @@ export default function Index() {
                   <ArticlePreview
                     category={
                       categories.find((c) => c.id === content.categoryId)
-                        ?.text ?? ""
+                      ?.text ?? ""
                     }
                     createdDate={content.createdAt.split("T")[0]}
                     title={content.title}
                     description={content.description}
                     url={content.url}
-                  />
+                    />
                 </div>
               </Card>
               {content.tags.length > 0 && (
@@ -69,5 +136,6 @@ export default function Index() {
         })}
       </div>
     </section>
+    </div>
   );
 }
