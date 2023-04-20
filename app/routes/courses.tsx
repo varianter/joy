43729169d6cdba5125e474517493460e594404a8
Sarah Serussi <sarah.serussi@gmail.com>
2023-04-20 10:56:41 +0200

    remove console.log

--- app/routes/courses.tsx
+++ app/routes/courses.tsx
@@ -16,22 +16,19 @@ const Course = () => {
       <h1 className="pb-8 text-white">Kurs</h1>
       <section className="max-w-5xl">
         <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          {courses?.map((course) => {
-            console.log("course", course);
-            return (
-              <div key={course.id} className="my-1 inline-grid">
-                <CourseCard
-                  title={course.title}
-                  linkToCourse={course.url}
-                  image={course.image}
-                  createdAt={course.createdAt}
-                  altImageText={course.imageText}
-                  description={course.description}
-                  id={course.id}
-                />
-              </div>
-            );
-          })}
+          {courses?.map((course) => (
+            <div key={course.id} className="my-1 inline-grid">
+              <CourseCard
+                title={course.title}
+                linkToCourse={course.url}
+                image={course.image}
+                createdAt={course.createdAt}
+                altImageText={course.imageText}
+                description={course.description}
+                id={course.id}
+              />
+            </div>
+          ))}
         </div>
       </section>
     </main>
