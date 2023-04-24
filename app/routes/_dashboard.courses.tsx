e7c48e4bfd0557d5d8bbe590125f717aa415c634
Sarah Serussi <sarah.serussi@gmail.com>
2023-04-24 10:59:43 +0200

    use CourseWithMultipleContent component instead of CourseCard

--- app/routes/_dashboard.courses.tsx
+++ app/routes/_dashboard.courses.tsx
@@ -1,38 +1,26 @@
-import CourseCard from "~/components/card/CourseCard";
-import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { getCourses } from "~/models/content.server";
+import CardWithMultipleContent from "~/components/card/CardWithMultipleContent";
+import { json } from "@remix-run/node";
 
 export const loader = async () => {
   const courses = await getCourses();
   return json({ courses });
 };
 
-const Course = () => {
+const Courses = () => {
   const { courses } = useLoaderData<typeof loader>();
 
   return (
-    <main className="flex flex-col items-center justify-center">
-      <h1 className="pb-8 text-white">Kurs</h1>
-      <section className="max-w-5xl">
-        <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          {courses?.map((course) => (
-            <div key={course.id} className="my-1 inline-grid">
-              <CourseCard
-                title={course.title}
-                linkToCourse={course.url}
-                image={course.image}
-                createdAt={course.createdAt}
-                altImageText={course.imageText}
-                description={course.description}
-                id={course.id}
-              />
-            </div>
-          ))}
-        </div>
-      </section>
-    </main>
+    <section className="flex max-w-5xl flex-col items-center justify-center">
+      <CardWithMultipleContent
+        content={courses}
+        contentType="course"
+        heading="Kurs"
+        buttonText="Gå til kurs"
+      />
+    </section>
   );
 };
 
-export default Course;
+export default Courses;
