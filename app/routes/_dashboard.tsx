b596f097f05d3e8f60f2b1c3f18739d2591bc081
Sarah Serussi <sarah.serussi@gmail.com>
2023-04-27 14:59:09 +0200

    more space over heading2

--- app/routes/_dashboard.tsx
+++ app/routes/_dashboard.tsx
@@ -41,7 +41,7 @@ export default function Dashboard() {
         </p>
       </section>
 
-      <section className="my-8 grid grid-cols-2 gap-5 md:grid-cols-4">
+      <section className="mt-8 mb-12 grid grid-cols-2 gap-5 md:grid-cols-4">
         <Link to="blogposts">
           <NavigationCard
             title={`Bloggposter (${numBlogposts})`}
