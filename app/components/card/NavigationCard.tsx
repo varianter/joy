608f223cf3ffc425bb226b16847fe948854b41e4
Mathilde Haukø Haugum <mathilde_haugum@hotmail.com>
2023-05-02 10:08:33 +0200

    Add dark icon and text state to navigation buttons

--- app/components/card/NavigationCard.tsx
+++ app/components/card/NavigationCard.tsx
@@ -9,8 +9,10 @@ const NavigationCard = (props: NavigationCardProps) => {
   return (
     <article
       className={`min-w-[10rem] overflow-hidden rounded-xl ${
-        isActive ? "bg-variant-blue" : "bg-variant-blue-2"
-      }  p-4 text-white hover:bg-variant-blue`}
+        isActive
+          ? "bg-variant-beige text-variant-dark-gray"
+          : "bg-variant-blue-2 text-white hover:bg-variant-blue"
+      }  p-4 `}
     >
       <h3 className="text-center text-lg">{title}</h3>
       <div className="flex justify-center">{icon}</div>
