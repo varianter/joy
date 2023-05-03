9eb314ad2b811dce02a7944af086c717911828a6
Mathilde Haukø Haugum <mathilde_haugum@hotmail.com>
2023-05-03 15:29:22 +0200

    Only show featured content on landing page

--- app/routes/_dashboard._index.tsx
+++ app/routes/_dashboard._index.tsx
@@ -1,13 +1,13 @@
 import { json } from "@remix-run/node";
-import { getNewestContent } from "~/models/content.server";
+import { getNewestFeaturedContent } from "~/models/content.server";
 import Featured from "~/components/Featured";
 import { useLoaderData, useRouteError } from "@remix-run/react";
 import ErrorComponent from "~/components/Error";
 
-const numberOfNewContent = 2;
+const numberOfNewContent = 4;
 
 export const loader = async () => {
-  const newestContent = await getNewestContent(numberOfNewContent);
+  const newestContent = await getNewestFeaturedContent(numberOfNewContent);
 
   return json({
     newestContent,
