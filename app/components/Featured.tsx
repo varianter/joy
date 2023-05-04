9c10782c90bbcf8dcaacae303421ed348a644d3f
Mathilde Haukø Haugum <mathilde_haugum@hotmail.com>
2023-05-04 14:20:19 +0200

    Change from CardWithContent to ArticlePreview

--- app/components/Featured.tsx
+++ app/components/Featured.tsx
@@ -4,21 +4,23 @@ import type { SerializeFrom } from "@remix-run/server-runtime";
 import ArticlePreview from "~/components/ArticlePreview";
 import TagButton from "~/components/buttons/TagButton";
 import Card from "~/components/card/Card";
-import { formatDate, getButtonText } from "~/utils";
-import CardWithContent from "./card/CardWithContent";
+import { formatDate } from "~/utils";
 
 interface FeaturedProps {
   newestFeaturedContent: SerializeFrom<(Content & { tags: Tag[] })[]>;
 }
 
 const Featured = ({ newestFeaturedContent }: FeaturedProps) => {
+  const horizontalFeaturedContent = newestFeaturedContent?.slice(0, 2);
+  const verticalFeaturedContent = newestFeaturedContent?.slice(2);
+
   return (
     <div>
       <section>
         <h3 className="mb-8 text-left text-white">Bli inspirert:</h3>
         <div>
-          {newestFeaturedContent?.slice(0, 2).map((content, index) => {
-            return (
+          <div className="grid grid-cols-1 gap-7">
+            {horizontalFeaturedContent?.map((content, index) => (
               <div key={content.id} className="my-5">
                 <Card cssClass="bg-variant-blue-4 sm:h-[22rem]">
                   <div className="grid h-full sm:grid-cols-2">
@@ -62,19 +64,48 @@ const Featured = ({ newestFeaturedContent }: FeaturedProps) => {
                   </div>
                 )}
               </div>
-            );
-          })}
-          <div className="grid gap-7 sm:grid-cols-1 md:grid-cols-2">
-            {newestFeaturedContent?.slice(2).map((content) => {
-              return (
-                <div key={content.id} className="my-5">
-                  <CardWithContent
-                    content={content}
-                    buttonText={getButtonText(content.category)}
-                  />
-                </div>
-              );
-            })}
+            ))}
+          </div>
+
+          <div className="grid grid-cols-1 gap-7 md:grid-cols-2">
+            {verticalFeaturedContent?.map((content) => (
+              <div key={content.id} className="my-5">
+                <Card cssClass="bg-variant-blue-4 sm:h-[32rem]">
+                  <div className="grid h-full">
+                    <div className="h-[12rem] sm:relative">
+                      <img
+                        alt={content.imageText ?? "Figur av læreglede"}
+                        className={`h-full w-full object-cover sm:absolute`}
+                        src={
+                          content.image ?? "/assets/default-article-image.svg"
+                        }
+                      />
+                    </div>
+
+                    <ArticlePreview
+                      category={content.category}
+                      createdDate={formatDate(new Date(content.createdAt))}
+                      title={content.title}
+                      description={content.description}
+                      url={content.url}
+                    />
+                  </div>
+                </Card>
+                {content.tags.length > 0 && (
+                  // TODO: Ved klikk på tags, så skal man finne all content relevant til denne
+                  <div className="flex justify-end gap-4">
+                    {content.tags.map((tag) => {
+                      return (
+                        <div key={tag.id} className="my-4">
+                          <TagButton text={tag.text} />
+                        </div>
+                      );
+                    })}
+                  </div>
+                )}
+              </div>
+            ))}
+            ;
           </div>
         </div>
       </section>
