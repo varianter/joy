40ec2b2d7bbefc2e2025baa804d129ffb8dc37b5
Mathilde Haukø Haugum <mathilde_haugum@hotmail.com>
2023-05-12 08:57:45 +0200

    Add slugify helper method

--- app/utils.ts
+++ app/utils.ts
@@ -61,3 +61,14 @@ export const getButtonText = (category: string) => {
       return "";
   }
 };
+
+export const createUrlSlug = (title: string): string => {
+  return title
+    .toLowerCase()
+    .replace(/æ/g, "ae")
+    .replace(/ø/g, "o")
+    .replace(/å/g, "a")
+    .replace(/[^a-z0-9]/g, "-")
+    .replace(/-+/g, "-")
+    .replace(/(^-)|(-$)/g, "");
+};
